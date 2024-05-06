1dd1709300def644cf3542f2304b79ce309df506
Aymen Sileshi Yirdaw <151214632+Aymen-s@users.noreply.github.com>
2024-05-06 21:34:34 +0300

    Time: 43 ms (86.76%) | Memory: 16.6 MB (73.27%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def commonChars(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        if len(words) <2:
+            return words
+        uniq=set(words[0])
+        list_t=[]
+        for  letter in uniq:
+            n=min(word.count(letter) for word in words)
+            if n:
+                list_t+=[letter]*n
+        return list_t
+        
